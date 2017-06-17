62d4bf15fc8f6d76cebc3f02c021c3e9bb1722f2
shado <shadowsvoices@gmail.com>
2017-06-17 18:12:41 +0200

    removed 7.0-debian specific build

--- 7.0-debian/startup/10-configure-nginx-from-env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Replace config placeholders with environment variables
-
-NGINX_WORKER_PROCESSES=${NGINX_WORKER_PROCESSES:=8}
-
-sed -i -e "s/__NGINX_WORKER_PROCESSES__/${NGINX_WORKER_PROCESSES}/" /etc/nginx/nginx.conf
